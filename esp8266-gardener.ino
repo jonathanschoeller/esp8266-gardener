c7579deb43b5a70fb96f89c81567c0cb87d6bdca
Jonathan Schoeller <jonathan.schoeller@gmail.com>
2017-12-20 20:11:02 +1100

    Initial commit

---
+++ esp8266-gardener.ino
@@ -0,0 +1,21 @@
+#include <AmazonIOTClient.h>
+#include <ESP8266AWSImplementations.h>
+#include <ESP8266WiFi.h>
+
+#include "keys.h"
+
+// keys.cpp contains
+// const char* awsKeyID = "";
+// const char* awsSecKey = "";
+// const char* ssid = "";
+// const char* password = "";
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ keys.h
@@ -0,0 +1,9 @@
+#ifndef KEYS_H_
+#define KEYS_H_
+
+extern const char* awsKeyID;
+extern const char* awsSecKey;
+
+extern const char* ssid;
+extern const char* password;
+#endif
